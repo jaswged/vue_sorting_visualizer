2186e05a89787396c4933cd5f8486b8a2a975a9d
Jaswged <jaswged@gmail.com>
2019-12-10 16:24:01 -0600

    fix row issue with bars data

--- src/components/SortingVisualizer.vue
+++ src/components/SortingVisualizer.vue
@@ -1,51 +1,58 @@
 <template>
   <v-container>
-    <v-layout
-      text-center
-      wrap
-    >
-      <v-btn target="_blank" outlined v-on:click="resetArray">
-        <span class="mr-2">Reset Array</span>
-      </v-btn>
-      <v-btn target="_blank" outlined>
-        <span class="mr-2">Merge Sort</span>
-      </v-btn>
-      <v-btn target="_blank" outlined>
-        <span class="mr-2">Sort!</span>
-      </v-btn>
-
-      <v-slider v-model="stateSize" label="Max error count" min="5" max="100" thumb-label="always" v-on:input="resetArray($event)"></v-slider>
-
-      <v-flex mb-5>
-        <h1 class="headline font-weight-bold mb-3">Hello world</h1>
-      </v-flex>
-
-      <v-layout justify-center>
-        <div className="array-container"
-          v-for="(val, kee) in state"
-          :key="kee"
-          class="subheading mx-3 array-bar"
-          target="_blank"
-        >
-          {{ val }}
-        </div>
-      </v-layout>
-
-    </v-layout>
+      <v-row>
+        <v-col cols="2">
+          <v-btn target="_blank" outlined v-on:click="resetArray">
+            <span class="mr-2">Reset Array</span>
+          </v-btn>
+        </v-col>
+        <v-col cols="2">
+          <v-btn target="_blank" outlined>
+            <span class="mr-2">Merge Sort</span>
+          </v-btn>
+        </v-col>
+        <v-col cols="2">
+          <v-btn target="_blank" outlined>
+            <span class="mr-2">Bubble Sort</span>
+          </v-btn>
+        </v-col>
+        <v-col cols="5">
+          <v-slider v-model="stateSize" label="Max error count" min="5" max="50" v-on:input="resetArray($event)"></v-slider>
+        </v-col>
+        <v-col cols ="1">
+          {{stateSize}}
+        </v-col>
+      </v-row>
+      <v-spacer/>
+      <v-row>
+        <v-col cols="12" justify="center">
+          <div className="array-container"
+            v-for="(val, kee) in state"
+            :key="kee"
+            class="subheading mx-3 array-bar"
+            target="_blank"
+            :style="{
+              backgroundColor: PRIMARY_COLOR,
+              height: `${val}px`,
+            }"
+          >
+          </div>
+        </v-col>
+      </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
   name: 'SortingVisualizer',
-
   data: () => ({
     state: [],
-    stateSize: 50
+    stateSize: 25,
+    PRIMARY_COLOR: 'turquoise',
+    SECONDARY_COLOR: 'red'
   }),
   methods: {
     randomIntFromInterval(min, max) {
-      // inclusive
       return Math.floor(Math.random() * (max - min + 1) + min);
     },
     resetArray(){
@@ -66,5 +73,9 @@ export default {
 </script>
 
 <style scoped>
-
+  .array-bar {
+      width: 3px;
+      display: inline-block;
+      margin: 0 1px;
+  }
 </style>
